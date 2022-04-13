4708580eaf70ac6ef805ad824536210385c8db78
gleehave <gleehave@gmail.com>
2022-04-13 16:10:47 +0900

    [이강호][modify] user 필드 추가

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ class User(TimeStampedModel):
     lectures      = models.ManyToManyField('lectures.Lecture', through='LectureUser')
     point         = models.IntegerField(default=1000000)
     kakao_id      = models.BigIntegerField()
+    kakao_nickname= models.CharField(max_length=100, default='bbooing')
     kakao_email   = models.CharField(max_length=100, null=True)
     profile_image = models.CharField(max_length=2000,
                                          default='https://media.istockphoto.com/photos/cute-blue-robot-giving-thumbs-up-3d-picture-id1350820098?b=1&k=20&m=1350820098&s=170667a&w=0&h=8gO4GcPH-wsEZS6PYn2WXbQN3ZPPv98vE6mBl-Ckwr8=')
