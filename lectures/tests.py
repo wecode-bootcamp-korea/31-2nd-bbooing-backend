03528bbed559ec92120af14d0b331328b38c2dcc
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-04-18 11:08:15 +0900

    [류미류][add] 카테고리 별 view/test  완료

--- lectures/tests.py
+++ lectures/tests.py
@@ -2,8 +2,79 @@ import json
 
 from django.test import TestCase, Client
 
-from lectures.models import Lecture, Category, Schedule, ScheduleLecture, Region, RegionLecture
+from lectures.models import Category, RegionLecture, ScheduleLecture, Type, Region, Schedule, Lecture, LectureImage, TypeLecture
 
+class LectureListViewTest(TestCase):
+    def setUp(self):
+        Category.objects.create(id = 1)
+        Type.objects.create(id = 1, type = '오프라인')
+        Type.objects.create(id = 2, type = 'VOD')
+        Region.objects.create(id = 1, region = '서울')
+        Schedule.objects.create(id = 1, schedule = '월요일')
+        Lecture.objects.create(
+            id                  = 1,
+            create_at           = '2022-01-01 00:00:00',
+            updated_at          = '2022-01-01 00:00:00',
+            category_id         = 1,
+            title               = '베인앤컴퍼니 출신의 논리적으로 일하는 법',
+            price               = '10000.00',
+            notice              = 'text',
+            summary             = 'text',
+            recommendation      = 'text',
+            thumbnail_image_url = 'https://bbooing.s3.ap-northeast-2.amazonaws.com/photo-1448523183439-d2ac62aca997.avif'
+        )
+
+        Lecture.objects.create(
+            id                  = 2,
+            create_at           = '2022-01-01 00:00:00',
+            updated_at          = '2022-01-01 00:00:00',
+            category_id         = 1,
+            title               = '기초부터 배우는 초단간 5분 드로잉',
+            price               = '50000.00',
+            notice              = 'text',
+            summary             = 'text',
+            recommendation      = 'text',
+            thumbnail_image_url = 'https://bbooing.s3.ap-northeast-2.amazonaws.com/photo-1448523183439-d2ac62aca997.avif'
+        )
+
+        LectureImage.objects.create(
+            id         = 1, 
+            lecture_id = 1, 
+            image_url  = 'https://bbooing.s3.ap-northeast-2.amazonaws.com/photo-1448523183439-d2ac62aca997.avif',
+            sequence   = 1
+        )
+
+        LectureImage.objects.create(
+            id         = 2, 
+            lecture_id = 2, 
+            image_url  = 'https://bbooing.s3.ap-northeast-2.amazonaws.com/photo-1448523183439-d2ac62aca997.avif',
+            sequence   = 1
+        )
+
+        RegionLecture.objects.create(id = 1, region_id = 1, lecture_id = 1)
+        RegionLecture.objects.create(id = 2, region_id = 1, lecture_id = 2)
+        ScheduleLecture.objects.create(id = 1, schedule_id = 1, lecture_id = 1)
+        ScheduleLecture.objects.create(id = 2, schedule_id = 1, lecture_id = 2)
+        TypeLecture.objects.create(id = 1, type_id = 1, lecture_id = 1)
+        TypeLecture.objects.create(id = 2, type_id = 2, lecture_id = 2)
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Type.objects.all().delete()
+        Region.objects.all().delete()
+        Schedule.objects.all().delete()
+        Lecture.objects.all().delete()
+        LectureImage.objects.all().delete()
+        RegionLecture.objects.all().delete()
+        ScheduleLecture.objects.all().delete()
+        TypeLecture.objects.all().delete()
+
+    def test_success_lecture_list_view_get(self):
+        client = Client() 
+        
+        response = client.get('/lectures?category_id=1&types_id=2')
+        self.assertEqual(response.json()['result'][0]['title'], '기초부터 배우는 초단간 5분 드로잉')
+        self.assertEqual(response.status_code, 200)
 
 class LectureDetailViewTest(TestCase):
     def setUp(self):
--- lectures/views.py
+++ lectures/views.py
@@ -1,7 +1,52 @@
+from django.db.models import Q, Count
+
 from django.http  import JsonResponse
 from django.views import View
 
 from lectures.models import Lecture, LectureImage
+
+class LectureListView(View):
+    def get(self, request):
+        category_id = request.GET.getlist('category_id', None)
+        types_id    = request.GET.getlist('types_id', None)
+        regions     = request.GET.getlist('regions', None)
+        schedules   = request.GET.getlist('schedules', None)
+        offset      = int(request.GET.get('offset', 0))
+        limit       = int(request.GET.get('limit', 12))
+
+        condition = Q()
+
+        if category_id:
+            condition &= Q(category_id__in = category_id)
+
+        if types_id:
+            condition &= Q(typelecture__type__id__in = types_id)
+    
+        if regions:
+            condition &= Q(regionlecture__region__region__in = regions)
+
+        if schedules:
+            condition &= Q(schedulelecture__schedule__schedule__in = schedules)
+
+        lectures = Lecture.objects.select_related('category')\
+                                  .prefetch_related('regions', 'schedules', 'types','lectureimage_set','lectureuser_set')\
+                                  .filter(condition).order_by('-id')[offset : offset + limit]
+
+        likes = Lecture.objects.values('id').annotate(likes_total = Count('lectureuser__user_id'))
+        
+        result = [{
+            'id'        : lecture.id,
+            'regions'   : lecture.regions.get().region,
+            'schedules' : lecture.schedules.get().schedule, 
+            'types'     : lecture.types.get().id,
+            'category'  : lecture.category.name,
+            'title'     : lecture.title,
+            'price'     : lecture.price,
+            'likes'     : likes[lecture.id-1]['likes_total'],
+            'images'    : [image.image_url for image in lecture.lectureimage_set.all()]
+        } for lecture in lectures]
+
+        return JsonResponse({'result' : result}, status = 200)
 
 class LectureDetailView(View):
     def get(self, request, lecture_id):
