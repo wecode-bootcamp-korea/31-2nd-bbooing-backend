cfb8c4a38131916e51c9200da754bbc9c219df9e
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-04-18 11:08:15 +0900

    [류미류][modify] 필터/검색 기능 피드백 반영 완료

--- lectures/urls.py
+++ lectures/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from lectures.views import LectureDetailView
+from lectures.views import LectureListView, LectureDetailView
 
 urlpatterns = [
+    path('/search', LectureListView.as_view()),
     path('/<int:lecture_id>', LectureDetailView.as_view()),
 ]
